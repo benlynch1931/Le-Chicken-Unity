7b32d962aed30358fef44ca4feacb47a0a54b5c7
Emily Sesto <emily@imac.home>
2020-12-08 09:48:25 +0000

    Create testing framework

---
+++ Assets/Tests/PlayMode/PlayerControllerTests.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+namespace Tests
+{
+    public class NewTestScript
+    {
+        
+        [UnityTest]
+        public IEnumerator MoveRight()
+        {
+            var gameObject = new GameObject();
+            var chicken = GameObject.AddComponent<PlayerController>();
+            chicken.Move(Input.GetAxisRaw("Horizontal") * moveSpeed * Time.deltaTime, 0f, 0f))
+            // Use the Assert class to test conditions.
+            // Use yield to skip a frame.
+            yield return null;
+        }
+    }
+}
